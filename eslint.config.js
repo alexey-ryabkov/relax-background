510bac8e7479257a6ac9d99171cc19d5f98ea909
alexey-ryabkov <xelaris@ya.ru>
2025-04-05 23:14:24 +0300

    chore: setup build n linting

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+const jsdoc = require('eslint-plugin-jsdoc');
+const prettier = require('eslint-plugin-prettier');
+
+module.exports = [
+  {
+    plugins: {
+      jsdoc,
+    },
+    rules: {
+      ...Object.fromEntries(
+        Object.entries(jsdoc.configs.recommended.rules).map(([key]) => [
+          key,
+          'warn',
+        ]),
+      ),
+    },
+  },
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+    },
+  },
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      jsdoc,
+      prettier,
+    },
+    rules: {
+      'accessor-pairs': 'warn',
+    },
+  },
+];
